46728fedb48a1b88bfbc1eea90aaa07d5ac3e7cf
Hênio Thuãn <heniothua13@gmail.com>
2022-02-15 10:05:07 -0300

    mais um pouco de logica

---
+++ ex5.py
@@ -0,0 +1,3 @@
+num = int(input("digite um número: "))
+ante = num -1
+print("O número escolhido foi {} e o seu antecessor é {}".format(num, ante))
---
+++ ex6.py
@@ -0,0 +1,4 @@
+base = float(input("Digite a base do seu retângulo: "))
+alt = float(input("Digite a altura do seu retângulo: "))
+area = base*alt
+print("A base do seu retângulo mede {}\n A altura do seu retãngulo é {}\n Logo a área do seu retângulo mede {:.2f} m²".format(base,alt,area))
