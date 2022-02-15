3f47c5768e4457592de894592fd85a7d72b41b10
Hênio Thuãn <heniothua13@gmail.com>
2022-02-15 09:09:23 -0300

    introdução a logica de programação

---
+++ ex2B.py
@@ -0,0 +1,8 @@
+a = 30
+b = 20
+c = a + b
+print("O valor de C é {}".format(c))
+b = 10
+print("O valor de B é {} e o valor de c é {}".format(b , c))
+c = a + b
+print("O valor de A é {}, o valor de b é {} e o valor de c {}".format(a , b , c ))
---
+++ ex2C.py
@@ -0,0 +1,6 @@
+a = 10
+b = 20
+c = a
+b = c
+a = b
+print("A {}, B {}, C {}".format(a, b, c))
