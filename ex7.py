7ff9cf2f9b9ed244b38a9a3de209309fd662f9de
Hênio Thuãn <heniothua13@gmail.com>
2022-02-15 15:24:55 -0300

    mais um pouco de logica

---
+++ ex7.py
@@ -0,0 +1,6 @@
+anos = int(input("digite sua idade em anos: "))
+meses = int(input("digite a sua idade em meses: "))
+dias= int(input("digite a sua idade em dias: "))
+anoDia = anos * 365
+mesDia = meses * 30
+print(" A sua idade em anos convertida para dias  é {}\n A  sua idade convertida de meses para dias é {}\n e a sua idade em dias é {}.".format(anoDia, mesDia, dias))
