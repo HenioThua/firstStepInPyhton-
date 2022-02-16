c077d0e373a5927fcd4cdeb99d2f078efbd7b39a
Hênio Thuãn <heniothua13@gmail.com>
2022-02-16 10:30:33 -0300

    logica

---
+++ ex13.py
@@ -0,0 +1,5 @@
+n1= float(input("Digite a primeira nota: "))
+n2= float(input("Digite a segunda nota: "))
+n3= float(input("digite a teceira nota: "))
+mediaFinal = (n1 * 2 + n2 *3 + n3 * 5) / 10
+print("A sua media final é {}".format(mediaFinal))
