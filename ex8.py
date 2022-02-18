6c295d556fca2d6848b239fdac64b7af261f0e78
Hênio Thuãn <heniothua13@gmail.com>
2022-02-18 10:10:34 -0300

    logica

--- ex8.py
+++ ex8.py
@@ -2,9 +2,9 @@ brancos = int(input("Quantidade de votos em brancos: "))
 nulos = int(input("Quantidade de votos nulos: "))
 validos = int(input("Quantidade de votos validos :"))
 eleitores = brancos + nulos + validos 
-validosPorcent= validos / eleitores * 100
-brancosPorcent= brancos / eleitores * 100
-nulosPorcent= nulos / eleitores * 100
+validosPorcent = validos / eleitores * 100
+brancosPorcent = brancos / eleitores * 100
+nulosPorcent = nulos / eleitores * 100
 print(" ELEITORES {}\n VOTOS EM BRANCO {:.2f}%\n VOTOS NULOS {:.2f}%\n VOTOS VALIDOS {:.2f}%".format(eleitores, brancosPorcent, nulosPorcent, validosPorcent ))
 
 
