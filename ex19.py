8da03ea37421be18ed37318cca021d1ad57e110f
Hênio Thuãn <heniothua13@gmail.com>
2022-02-18 10:11:10 -0300

    logica

---
+++ ex19.py
@@ -0,0 +1,6 @@
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite um número: "))
+if num1 > num2:
+    print("O maior valor é {}".format(num1))
+else:
+    print("O maior valor é {}".format(num2))
