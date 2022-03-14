747d328851d8c0f53a14a28d539f5bedafe73b9c
Hênio Thuãn <heniothua13@gmail.com>
2022-03-14 18:42:29 -0300

    logica

--- ex65.py
+++ ex65.py
@@ -2,6 +2,6 @@ soma = 0
 valor_1 = int(input("Digite o primeiro valor: "))
 valor_2 = int(input("Digite o segundo valor: "))
 for i in range(valor_1, valor_2 + 1):
-    soma = valor_1 + i + valor_2
-    print(soma)
+    soma += i
+print(soma)
     
