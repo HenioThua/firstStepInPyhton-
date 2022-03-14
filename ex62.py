37e8e52fdb305f09046e2a774e99c801459289e0
Hênio Thuãn <heniothua13@gmail.com>
2022-03-14 18:19:34 -0300

    logica

--- ex62.py
+++ ex62.py
@@ -1,7 +1,7 @@
 nota_total = 0
 alunos = int(input("Digite o número de alunos: "))
 for i in range(1, alunos + 1):
-    notas = float(input("digite a sua nota: "))
-    nota_total += notas
-    media = nota_total / alunos
+    nota = float(input("digite a sua nota: "))
+    nota_total += nota
+media = nota_total / alunos
 print("a media final da turma é {:.2f}".format(media))
