79ae9bc6167b2a92007a281fd80549635e6d83aa
Hênio Thuãn <heniothua13@gmail.com>
2022-04-18 16:15:31 -0300

    logica

--- ex81.py
+++ ex81.py
@@ -1,12 +1,12 @@
 q = []
 valor = int(input("digite um valor: "))
 q.append(valor)
-maior = valor
+menor = valor
 posicao = 0 
 for i in range(1, 20):
     valor = int(input("digite um valor: "))
-    if valor > maior:
-        maior = valor
+    if valor < menor:
+        menor = valor
     q.append(valor)
-    posicao = q.index(maior)
-print(q, maior, posicao)
+    posicao = q.index(menor)
+print(q, menor, posicao)
